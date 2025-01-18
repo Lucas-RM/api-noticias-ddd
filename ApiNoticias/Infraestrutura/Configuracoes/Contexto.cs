8dd2d888d8a1bbc3e6d0f31d33edabe2c6cb8f5d
Lucas-RM <lucas.marcondes36@gmail.com>
2025-01-18 18:30:13 -0300

    Criação Domínio e Infraestrutura

---
+++ ApiNoticias/Infraestrutura/Configuracoes/Contexto.cs
@@ -0,0 +1,36 @@
+﻿using Entidades.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infraestrutura.Configuracoes
+{
+    public class Contexto : IdentityDbContext<ApplicationUser>
+    {
+        public Contexto(DbContextOptions<Contexto> opcoes) : base(opcoes) { }
+
+        public DbSet<Noticia> Noticia { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+        public string ObterStringConexao()
+        {
+            string stringConexao = "Data Source=LUCAS\\DESENVOLVIMENTO;Initial Catalog=API_NOTICIAS_DDD;Integrated Security=False;User ID=sa;Password=LU5830mar03*;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+            return stringConexao;
+        }
+    }
+}
---
+++ ApiNoticias/Infraestrutura/Repositorio/RepositorioUsuario.cs
@@ -0,0 +1,46 @@
+﻿using Dominio.Interfaces;
+using Entidades.Entidades;
+using Infraestrutura.Configuracoes;
+using Infraestrutura.Repositorio.Genericos;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace Infraestrutura.Repositorio
+{
+    public class RepositorioUsuario : RepositorioGenerico<ApplicationUser>, IUsuario
+    {
+        private readonly DbContextOptions<Contexto> _optionsBuilder;
+
+        public RepositorioUsuario()
+        {
+            _optionsBuilder = new DbContextOptions<Contexto>();
+        }
+
+        public async Task<bool> AdicionaUsuario(string email, string senha, int idade, string celular)
+        {
+            try
+            {
+                using (var banco = new Contexto(_optionsBuilder))
+                {
+                    await banco.ApplicationUser.AddAsync(
+                        new ApplicationUser
+                        {
+                            Email = email,
+                            PasswordHash = senha,
+                            Idade = idade,
+                            Celular = celular
+                        });
+
+                    await banco.SaveChangesAsync();
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
